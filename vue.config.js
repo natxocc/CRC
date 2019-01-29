2541619173b432604f8584300e295c2fa3f273d2
natxocc <natxocc@gmail.com>
2019-01-29 17:47:44 +0100

    remove webpack from vue

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,5 @@ module.exports = {
   publicPath: './',
   assetsDir: 'reale',
   productionSourceMap: false,
-  lintOnSave: false,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'vue$': 'vue/dist/vue.min.js'
-      }
-    },
-  }
+  lintOnSave: false
 }
